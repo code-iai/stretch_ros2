7eb9275edf9499d11879fd5a68b6a8b41ee40925
stretch-ext <stretch@tiago-ext.com>
2026-03-30 14:53:37 +0200

    test gripper interface

--- stretch_core/stretch_core/stretch_driver.py
+++ stretch_core/stretch_core/stretch_driver.py
@@ -215,7 +215,7 @@ class StretchDriver(Node):
             if 'stretch_gripper' in self.robot.end_of_arm.joints:
                 pos = qvel[Idx.GRIPPER]
                 print("SET GRIPPER: ", pos)
-                self.robot.end_of_arm.move_to('stretch_gripper', pos)
+                self.robot.end_of_arm.get_joint('stretch_gripper').set_velocity(pos)
             
             # self.get_logger().info(f"Moved at velocity qvel: {qvel}")
         except Exception as e:
